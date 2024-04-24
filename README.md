f5b0d5b1e77b488c6fa2e896bc38548178aeb3a0
Sweaty Hog <Royceatkins93@gmail.com>
2024-04-24 15:12:19 -0400

    added MD section and function to generate the read me. also added fs write file to create the md file.

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Example Read Me [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+Showing off the ability to make readmes through terminal.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+Start up program and run node index.js in terminal
+
+## Usage
+Fill out each question to insert into your read me
+
+## License
+This project is licensed under the MIT license.
+
+
+## Questions
+Here is the link to my git hub!
+https://github.com/Wartech93
+
+You can reach me at: 
+royceatkins93@gmail.com
+
--- index.js
+++ index.js
@@ -22,3 +22,68 @@ const licenseChoices = [
         choices: licenseChoices
     }
 ];      
+//assigns license to licensebadge
+function generateReadme() {
+    inquirer.prompt(questions).then(answers => {
+  
+            let licenseBadge = "";
+
+            // Define license badges
+            switch (answers.license) {
+              case "MIT":
+                licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+                break;
+              case "Apache-2.0":
+                licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+                break;
+              case "GNU GPLv3":
+                licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+                break;
+              default:
+                licenseBadge = "";
+            }
+//content for readme to be created as
+            const readmeContent = `
+# ${answers.title} ${licenseBadge}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+
+## Questions
+Here is the link to my git hub!
+https://github.com/${answers.github}
+
+You can reach me at: 
+${answers.email}
+
+`;
+
+            const fileName = 'README.md';
+
+            fs.writeFile(fileName, readmeContent, (err) => {
+              if (err) {
+                console.error('Error writing README file:', err);
+              } else {
+                console.log(`README file generated: ${fileName}`);
+              }
+            });
+          });
+        };
+        //call generateratereadme function
+generateReadme();
